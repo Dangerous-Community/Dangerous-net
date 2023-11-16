d144490b1e7e13ce4107e7208060195e2ad38e65
Anthony Grace <111553838+SATUNIX@users.noreply.github.com>
2023-11-16 19:18:12 +1000

    Create keycard_link.go

---
+++ src/libb/keycard_link.go
@@ -0,0 +1,17 @@
+//
+// Library:
+//    /src/libb/***
+// Program:
+//    Keycard link between IPFSS and the official Keycard Go API
+//    Possibly place the Go API in /src/libb/API or use git to retrieve the API at build for efficiency
+
+
+package main 
+
+import package (
+
+  )
+
+func main (){
+
+}
