e9b7014e5e0c8449318b7f2709ec4273c0fc5556
Anthony Grace <111553838+SATUNIX@users.noreply.github.com>
2023-11-16 21:36:02 +1000

    Update keycard_link.go

--- src/libb/keycard_link.go
+++ src/libb/keycard_link.go
@@ -5,8 +5,8 @@ import (
     "fmt"
     "os"
 
-    // Import the keycard-go package
     keycard "github.com/status-im/keycard-go"
+    // Other imports required for Keycard interaction
 )
 
 // askPassphrase prompts the user for a passphrase and returns it
@@ -22,13 +22,23 @@ func askPassphrase() (string, error) {
 
 // callKeycardAPI signs the given passphrase using the Keycard API
 func callKeycardAPI(passphrase string) (string, error) {
-    // Here, you'll need to implement the logic to interact with the Keycard API.
-    // This will involve establishing a connection to the Keycard usinga PCSC reader and sending
-    // the passphrase for signing. The specifics will depend on the Keycard API's functionality.
-    //
-    // As a placeholder:
-    signature := "signed_" + passphrase // Replace this with actual Keycard API interaction
-    return signature, nil
+    // Convert the passphrase to a byte slice
+    passphraseBytes := []byte(passphrase)
+
+    // Initialize your CommandSet with Keycard session
+    // This typically involves setting up communication with the Keycard
+    // using a PCSC reader or other means, as per Keycard API requirements
+    cs := /* Initialize CommandSet with Keycard session */
+
+    // Sign the passphrase with the Keycard
+    signature, err := cs.Sign(passphraseBytes)
+    if err != nil {
+        return "", fmt.Errorf("error signing passphrase with Keycard: %w", err)
+    }
+
+    // Convert the signature to a string or a format suitable for your application
+    signatureString := fmt.Sprintf("%x", signature) // Example: Convert to hexadecimal
+    return signatureString, nil
 }
 
 // EncryptFile encrypts the given file using GPG with a passphrase
@@ -43,9 +53,9 @@ func EncryptFile(filename string) error {
         return fmt.Errorf("error signing passphrase: %w", err)
     }
 
-    // Here, you'll implement the GPG encryption logic using the signed passphrase
-    // as the encryption key. This might involve calling an external GPG command
-    // or using a Go package that provides GPG functionality.
+    // Implement the GPG encryption logic using the signed passphrase
+    // This might involve calling an external GPG command
+    // or using a Go package that provides GPG functionality
 
     // Placeholder for GPG encryption:
     fmt.Printf("File %s encrypted with signature %s\n", filename, signature)
@@ -65,7 +75,7 @@ func DecryptFile(filename string) error {
         return fmt.Errorf("error signing passphrase: %w", err)
     }
 
-    // Implement the GPG decryption logic here, similar to EncryptFile.
+    // Implement the GPG decryption logic here, similar to EncryptFile
 
     // Placeholder for GPG decryption:
     fmt.Printf("File %s decrypted with signature %s\n", filename, signature)
