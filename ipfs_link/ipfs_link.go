5c65272c9fdd32922e5f3198467c3654c26dd00a
Anthony Grace <111553838+SATUNIX@users.noreply.github.com>
2023-12-07 15:27:12 +1000

    Straight to 1.1
    I did alot of changes before updating it here, Added a loading screen when uploading and downloading. 
    That took alot of time to process the ascii animation.

--- ipfs_link/ipfs_link.go
+++ ipfs_link/ipfs_link.go
@@ -5,16 +5,21 @@ import (
     "fmt"
     "os/exec"
     "strings"
+    "IPFSS_IPFS-Secure/art_link"
 )
 
 // AddFileToIPFS adds a file to IPFS and returns the CID (Content Identifier)
 func AddFileToIPFS(filePath string) (string, error) {
+    done := make(chan bool)
+    go art_link.LoadingScreen(done) // Start the loading screen in a separate goroutine
+
     cmd := exec.Command("ipfs", "add", filePath)
 
     var out bytes.Buffer
     cmd.Stdout = &out
     err := cmd.Run()
     if err != nil {
+        done <- true // Signal to stop the loading screen on error
         return "", fmt.Errorf("error adding file to IPFS: %w", err)
     }
 
@@ -25,22 +30,27 @@ func AddFileToIPFS(filePath string) (string, error) {
         if strings.Contains(line, "added") {
             parts := strings.Fields(line)
             if len(parts) >= 2 {
+                done <- true // Signal to stop the loading screen on successful add
                 return parts[1], nil // Assuming the CID is the second part
             }
         }
     }
 
+    done <- true // Signal to stop the loading screen if CID is not found
     return "", fmt.Errorf("CID not found in IPFS add output")
 }
-
 // GetFileFromIPFS retrieves a file from IPFS using its CID
 func GetFileFromIPFS(cid, outputPath string) error {
-    cmd := exec.Command("ipfs", "get", cid, "-o", outputPath)
+    done := make(chan bool)
+    go art_link.LoadingScreen(done) // Start the loading screen in a separate goroutine
 
+    cmd := exec.Command("ipfs", "get", cid, "-o", outputPath)
     err := cmd.Run()
     if err != nil {
+        done <- true // Signal to stop the loading screen on error
         return fmt.Errorf("error retrieving file from IPFS: %w", err)
     }
 
+    done <- true // Signal to stop the loading screen on successful download
     return nil
 }
